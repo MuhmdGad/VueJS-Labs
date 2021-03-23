06f4e2f4ea3a84c55f0045931f46f9b5312b7ae7
Muhammad Taha <mohamedengineer944@gmail.com>
2021-03-24 01:54:38 +0200

    VueJs Lab 01

---
+++ js/books.js
@@ -0,0 +1,104 @@
+const books = [
+    {
+        "pages": 36,
+        "author": "ITI",
+        "category": "comics",
+        "title": "Pentaho Analytics for MongoDB",
+        "subtitle": "Combine Pentaho Analytics and MongoDB to create powerful analysis and reporting solutions",
+        "isbn": "9781782168355",
+        "price": "16.99",
+        "image": "https://itbook.store/img/books/9781782168355.png",
+        "url": "https://itbook.store/books/9781782168355"
+    },
+    {
+        "pages": 84,
+        "author": "Mohammed",
+        "category": "romantic",
+        "title": "Pentaho Analytics for MongoDB Cookbook",
+        "subtitle": "Over 50 recipes to learn how to use Pentaho Analytics and MongoDB to create powerful analysis and reporting solutions",
+        "isbn": "9781783553273",
+        "price": 35.99,
+        "image": "https://itbook.store/img/books/9781783553273.png",
+        "url": "https://itbook.store/books/9781783553273",
+        
+
+    },
+    {
+        "pages": 588,
+        "author": "Muhammad",
+        "category": "manga",
+        "title": "The Definitive Guide to MongoDB, 3rd Edition",
+        "subtitle": "A complete guide to dealing with Big Data using MongoDB",
+        "isbn": "9781484211830",
+        "price": 49.99,
+        "image": "https://itbook.store/img/books/9781484211830.png",
+        "url": "https://itbook.store/books/9781484211830"
+    },
+    {
+        "pages": 15,
+        "author": "Ahmed",
+        "category": "child",
+        "title": "MongoDB in Action, 2nd Edition",
+        "subtitle": "Covers MongoDB version 3.0 and has a reallife examples",
+        "isbn": "9781617291609",
+        "price": 19.99,
+        "image": "https://itbook.store/img/books/9781617291609.png",
+        "url": "https://itbook.store/books/9781617291609"
+    },
+
+    {
+        "pages": 10,
+        "author": "abdelrahman",
+        "category": "movies",
+        "title": "Web Development with MongoDB and NodeJS, 2nd Edition",
+        "subtitle": "Build an interactive and full-featured web application from scratch using Node.js and MongoDB",
+        "isbn": "9781785287527",
+        "price": 38.01,
+        "image": "https://itbook.store/img/books/9781785287527.png",
+        "url": "https://itbook.store/books/9781785287527"
+    },
+    {
+        "pages": 68,
+        "author": "it books",
+        "category": "MongoDB",
+        "title": "MongoDB Cookbook, 2nd Edition",
+        "subtitle": "Harness the latest features of MongoDB 3 with this collection of 80 recipes - from managing cloud platforms to app development, this book is a vital resource",
+        "isbn": "9781785289989",
+        "price": 44.99,
+        "image": "https://itbook.store/img/books/9781785289989.png",
+        "url": "https://itbook.store/books/9781785289989"
+    },
+    {
+        "pages": 896,
+        "author": "Eng Noha",
+        "category": "MongoDB",
+        "title": "The Little MongoDB Book",
+        "subtitle": "Harness the latest features of MongoDB 3 with this collection of 80 recipes",
+        "isbn": "1001592208320",
+        "price": 30.62,
+        "image": "https://itbook.store/img/books/1001592208320.png",
+        "url": "https://itbook.store/books/1001592208320"
+    },
+    {
+        "pages": 885,
+        "author": "Eng Nasr",
+        "category": "Node.js",
+        "title": "Node.js, MongoDB and Angular Web Development, 2nd Edition",
+        "subtitle": "The definitive guide to using the MEAN stack to build web applications",
+        "isbn": "9780134655536",
+        "price": 33.16,
+        "image": "https://itbook.store/img/books/9780134655536.png",
+        "url": "https://itbook.store/books/9780134655536"
+    },
+    {
+        "pages": 186,
+        "author": "Taha",
+        "category": "MongoDB",
+        "title": "The Definitive Guide to MongoDB",
+        "subtitle": "The NoSQL Database for Cloud and Desktop Computing",
+        "isbn": "9781430230519",
+        "price": 39.99,
+        "image": "https://itbook.store/img/books/9781430230519.png",
+        "url": "https://itbook.store/books/9781430230519"
+    }
+]
